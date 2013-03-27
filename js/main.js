5ac49ff976ed185f7dc56539ee21a077a090f2d9
Patrick Morris <patrickjohnmorris@gmail.com>
2013-03-26 21:17:47 -0400

    cleanup homepage

--- js/main.js
+++ js/main.js
@@ -1,12 +1,80 @@
-;(function ($, window, undefined) {
-  'use strict';
+var isMobile;
 
-  $(document).ready(function() {
-    $.localScroll.defaults.axis = 'y';
-    $.localScroll();
+// Identify if visitor on mobile with lame sniffing to remove parallaxing title
+if( navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/BlackBerry/)
+){
+  isMobile = true;
+}
+
+$(document).ready(function() {
+
+  // Global vars
+  var $mainHeaderInner = $('.main-header-inner');
+  var $mainHeader = $('.main-header');
+  var $mainTitle = $('.main-title');
+  var $mainSubtitle = $('.main-subtitle');
+  var mainTitleFontSize = parseInt($mainTitle.css('font-size'));
+  var $nav = $('.nav');
+  var windowScroll;
+
+  // Apply Fittext to titles to make it scale responsively in a smooth fashion
+  //$mainTitle.fitText(1, { minFontSize: '34px' });
+
+  // Identify if visitor has a large enough viewport for parallaxing title
+  function isLargeViewport() {
+    if($nav.css('position') == "relative") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // If large viewport and not mobile, parallax the title
+  if(!isMobile) {
+    $(window).scroll(function() {
+      if(isLargeViewport()) {
+        slidingTitle();
+      }
+    });
+  }
+
+  // Window gets large enough, need to recalc all parallaxing title values
+  $(window).resize(function() {
+    if(isLargeViewport()) {
+      slidingTitle();
+    }
   });
 
+  // Functional parallaxing calculations
+  function slidingTitle() {
+    //Get scroll position of window
+    windowScroll = $(this).scrollTop();
 
-})
+    //Slow scroll of .main-header-inner scroll and fade it out
+    $mainHeaderInner.css({
+      'margin-top' : -(windowScroll/3)+"px",
+      'opacity' : 1-(windowScroll/550)
+    });
 
-(jQuery, this);
+    //Slowly parallax the background of .main-header
+    $mainHeader.css({
+      'background-position' : 'center ' + (-windowScroll/8)+"px"
+    });
+
+    //Fade the .nav out
+    $nav.css({
+      'opacity' : 1-(windowScroll/400)
+    });
+  }
+
+  // Link to top of page without changing URL
+  $('.back-to-top a').click(function(e) {
+    e.preventDefault();
+    $(window).scrollTop(0);
+  })
+});
